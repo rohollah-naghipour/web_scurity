0aaf568d9a8969811db1ae94851a481ba6486636
rohy <rohollahnaghipour41@gmail.com>
2025-10-15 16:37:31 +0330

    setup signup/login and form with template

--- blog/urls.py
+++ blog/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     path('post_list/',  views.post_list, name='post_list'),
     path('post-detail/<int:pk>/', views.post_detail, name='post_detail'),
-
-    #path('signup/', views.signup_view, name='signup'),
-    #path('login/', views.login_view, name='login'),
-    #path('logout/', views.logout_view, name='logout'),
+    path('signup/', views.register_view ,name='register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+   
 ]
--- blog/views.py
+++ blog/views.py
@@ -7,8 +7,9 @@ from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
+from django.http import HttpResponseRedirect
 
-
+from blog.forms import UserRegisterForm, UserLoginForm 
 
 def post_list(request):
     posts = Post.objects.all()
@@ -17,3 +18,38 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'post_detail.html', {'post': post})
+
+
+def register_view(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Registration was successful! You can now log in.')
+            return redirect('login')
+        else:
+            messages.error(request, 'Please check error form')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'register.html', {'form': form})
+
+    
+
+def login_view(request):
+    if request.method == 'POST':
+        form = UserLoginForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            print("user = ", user)
+            print("request.user = ", request.user)
+            login(request, user)
+            return redirect('post_list')
+        else:
+            messages.error(request, 'The username or password is incorrect.')
+    else:
+        form = UserLoginForm()
+    return render(request, 'login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')    
